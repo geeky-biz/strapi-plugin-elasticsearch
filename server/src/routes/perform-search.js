6bd29f8712ac71ddbbbee797768ca92a9c9e54b3
Leftovers.today <139936478+LeftoversTodayAppAdmin@users.noreply.github.com>
2025-04-29 17:35:48 -0700

    Update perform-search.js to also include all the indexing APIs so they can be called with an API Token instead of just through the admin UI.
    More details can be found here: https://github.com/geeky-biz/strapi-plugin-elasticsearch/issues/9

--- server/src/routes/perform-search.js
+++ server/src/routes/perform-search.js
@@ -9,6 +9,24 @@ module.exports = {
         config: { 
             policies: []
         },
-      }
+      },
+      {
+        method: 'GET',
+        path: '/reindex',
+        handler: 'performIndexing.rebuildIndex',
+        config: { policies: [] },
+      },
+      {
+        method: 'GET',
+        path: '/collection-reindex/:collectionname',
+        handler: 'performIndexing.indexCollection',
+        config: { policies: [] },
+      },
+      {
+        method: 'GET',
+        path: '/trigger-indexing/',
+        handler: 'performIndexing.triggerIndexingTask',
+        config: { policies: [] },
+      },
     ],
   };
